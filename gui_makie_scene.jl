deb2636a29d042e21c14776c62a31584982cfa7c
SinixND <freak105@gmx.de>
2022-08-25 00:25:20 +0200

    arch

--- gui_makie_scene.jl
+++ gui_makie_scene.jl
@@ -1,48 +1,21 @@
-using Makie
-using AbstractPlotting
+using GLMakie, Makie
+scene = Scene(
+    #clear everything behind scene
+    clear = true,
+    # the camera struct of the scene.
+    visible = true,
+    # ssao and light are explained in more detail in `Documetation/Lighting`
+    ssao = Makie.SSAO(),
+    # Creates lights from theme, which right now defaults to `
+    # set_theme!(lightposition=:eyeposition, ambient=RGBf(0.5, 0.5, 0.5))`
+    lights = Makie.automatic,
+    backgroundcolor = :gray,
+    resolution = (500, 500);
+    # gets filled in with the currently set global theme
+    theme_kw...
+)
 
-println(“How many generations would you like to see?”)
-g=parse(Int,readline())
-#readline() reads the keyboard and parse(Int,variable/value) transforms it into an integer
-
-println(“How many columns /rows would you like to see?”)
-size=parse(Int,readline())
-Life =round.(rand(size,size))
-
-#Life =[[0.0,0.0,0.0,0.0,0.0] [0.0,0.0,1.0,0.0,0.0] [0.0,0.0,1.0,0.0,0.0] [0.0,0.0,1.0,0.0,0.0] [0.0,0.0,0.0,0.0,0.0]]
-
-Life=[zeros(size)’;Life;zeros(size)’]
-Life=[zeros(size+2) Life zeros(size+2)]scena=heatmap!(Life,colormap=:binary)
-st = Stepper(scena, “generation”)
-step!(st)
-
-function generation(Lifein)
-	#initialize 
-	NewLifein=zeros(size+2,size+2) 
-	for colJump in size+4:size+2:(size+2)*(size+2)-(size+2)-size
-		for rowWalk in 0:size-1
-			i=colJump+rowWalk
-			sum=0
-			sum=Lifein[i-8]+Lifein[i+8]+Lifein[i+7]+Lifein[i-7]+Lifein[i+6]+Lifein[i-6]+Lifein[i-1]+Lifein[i+1]
-			if Lifein[i]==1.0&&1<sum<4 
-				NewLifein[i]=1.0
-			elseif Lifein[i]==0&&sum==3.0 
-				NewLifein[i]=1.0
-			end
-		end
-	end
-	return NewLifein 
-end
-
-for counter in 1:g
-	#this generates new generation
-	Life=generation(Life) 
-	#this clears the board
-	scena.clear 
-	#this generates new picture
-	scena=heatmap!(Life,colormap=:binary) 
-	scena.strokewidth=20
-	#this saves new picture with increment name
-	step!(st) 
-end
-
+GLMakie.activate!()
+scene = Scene(backgroundcolor=:gray)
+subwindow = Scene(scene, px_area=Rect(100, 100, 200, 200), clear=true, backgroundcolor=:white)
+scene
