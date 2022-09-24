95ab49a24cda5a542efaa34df6e6c3781043b8e0
SinixND <freak105@gmx.de>
2022-09-24 23:38:07 +0000

    cpp working anno

--- anno1800calculator/src/main.cpp
+++ anno1800calculator/src/main.cpp
@@ -1,8 +1,24 @@
-#include <iostream>
+#include <string>
 
 using namespace std;
 
+class production_building{
+	public: 
+		string category; //city, corsumables, material
+		int upkeep;
+		string resident_tier; //farmer, worker, artisan, engineer
+		int workforce;
+		int residents_supplied;
+};
+
+class house{
+	public:
+		string resident_tier;
+		int residents;
+};
+
 int main() {
-	cout << "Hello World!" << endl;
+	production_building sawmill;
+	
 	return 0;
 }
