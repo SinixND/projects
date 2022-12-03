1e133e8914fd135976bf9a0587865eaa867f1d56
SinixND <freak105@gmx.de>
2022-12-03 18:24:47 +0100

    discarded configs

---
+++ wxW/wxW_hardcoded/include/classes.h
@@ -0,0 +1,24 @@
+#ifndef CLASSES_H
+#define CLASSES_H
+
+#include <string>
+#include <iostream>
+
+class Klasse{
+private:
+	std::string Attribut;
+
+public:
+	void setAttribut(std::string attribut);
+	
+	std::string getAttribut(); //one per attribute
+	
+	void Methode();
+
+	//constructor
+	Klasse(std::string attribut);
+	//destructor
+	~Klasse();
+};
+
+#endif
