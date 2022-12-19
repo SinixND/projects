ce64e145bd7fd0e617d634813ad8fae9b72b68a2
SinixND <freak105@gmx.de>
2022-12-19 00:59:40 +0000

    added mnemonic and accelerator to menus

--- gui_loadout_builder/src/cFrames.cpp
+++ gui_loadout_builder/src/cFrames.cpp
@@ -16,13 +16,13 @@ cFrameMain::cFrameMain(const wxString &rTitle) : wxFrame(NULL, wxID_ANY, rTitle,
     pm_menuHelp = new wxMenu;
 
     // append entries to the menus
-    pm_menuFile->Append(ID_Hello, wxT("&New"), wxT("Clear loadout"));
-    pm_menuFile->Append(ID_Hello, wxT("&Open"), wxT("Open a saved loadout"));
-    pm_menuFile->Append(ID_Hello, wxT("&Save", wxT("Save current loadout")));
-    pm_menuFile->Append(ID_Hello, wxT("&Save As"), wxT("Save a new loadout"));
+    pm_menuFile->Append(ID_Hello, wxT("&New/tStrg-N"), wxT("Clear loadout"));
+    pm_menuFile->Append(ID_Hello, wxT("&Open/tStrg-O"), wxT("Open a saved loadout"));
+    pm_menuFile->Append(ID_Hello, wxT("Save/tStrg-S", wxT("Save current loadout")));
+    pm_menuFile->Append(ID_Hello, wxT("&Save As/tStrg-Shift-S"), wxT("Save a new loadout"));
     pm_menuFile->AppendSeparator();
-    pm_menuFile->Append(wxID_EXIT, wxT("&Quit"), wxT("Exit the program")); //no label necessary
-    pm_menuHelp->Append(wxID_ABOUT, wxT("&About"), wxT("Show the About Information")); //no label necessary
+    pm_menuFile->Append(wxID_EXIT, wxT("E&xit/tStrg-Q"), wxT("Exit the program")); //no label necessary
+    pm_menuHelp->Append(wxID_ABOUT, wxT("&About..."), wxT("Show the About Information")); //no label necessary
 
     // append the menus to the menu bar
     pm_menuBar->Append(pm_menuFile, "&File");
@@ -31,8 +31,9 @@ cFrameMain::cFrameMain(const wxString &rTitle) : wxFrame(NULL, wxID_ANY, rTitle,
     // attach menu bar to the frame
     SetMenuBar(pm_menuBar);
 
-    // create a status bar
-    CreateStatusBar();
+    // create a status bar with 2 fields
+    CreateStatusBar(2);
+	// set first fields default
     SetStatusText("Additional information displayed here");
 };
 
