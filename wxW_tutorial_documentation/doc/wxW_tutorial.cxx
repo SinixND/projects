d6a72c4d46439bdb4d8a10364494557430d0dcd0
SinixND <freak105@gmx.de>
2022-12-21 21:15:07 +0100

    Added wxWidgets tutorial folder with GUI documentation file

---
+++ wxW_tutorial_documentation/doc/wxW_tutorial.cxx
@@ -0,0 +1,52 @@
+//////// =============================================================
+//////// =============================================================
+//////// Tutorial on how to make a wxWidgets-GUI application
+//////// Code is split in declaration (.h) and implementation (.cpp)
+//////// with generic file names (MyXX) to avoid confusion with class 
+//////// names. No file separation needed. From my expirience 
+//////// application declaration and implementation have to be at the
+//////// end for single file GUIs.
+//////// =============================================================
+//////// =============================================================
+
+////// ===============================================================
+////// Minimal required
+////// ===============================================================
+
+//// DECLARATION MyApp.h
+#include <wx/wxprec.h>
+
+#ifndef WX_PRECOMP
+	#include <wx/wx.h>
+#endif //WX_PRECOMP
+
+class App : public wxApp{
+public:
+	virtual bool OnInit();
+};
+
+//// IMPLEMENTATION MyApp.cpp
+wxIMPLEMENT_APP(App);
+
+bool App::OnInit(){
+    return true;
+}
+
+////// ===============================================================
+////// Add a frame
+////// ===============================================================
+
+//// DECLARATION MyFrame.h
+class MyFrame : public wxFrame{
+public:
+	MyFrame(const wxString &rTitle);
+};
+
+//// IMPLEMENTATION MyFrame.cpp
+MyFrame::MyFrame(const wxString &rTitle) 
+    : wxFrame(NULL, wxID_ANY, rTitle, wxDefaultPosition, wxSize(640, 320)){
+};
+
+//// ADD TO OnInit() in MyApp.cpp
+MyFrame *pm_MyFrame = new MyFrame("MAIN-WINDOW-TITLE");
+pm_MyFrame->Show(true);
