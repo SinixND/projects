b90400e3cd9d0e23ec023f64ddea118f8f01937a
SinixND <freak105@gmx.de>
2023-01-03 17:24:45 +0100

    edited specialisationis_data.h

---
+++ gui_loadout_builder/include/specialisations_data.h
@@ -0,0 +1,24 @@
+#include <string>
+
+class Specialisation {
+private:
+    Specialisation(string &rarg){
+        mName = rarg;
+    };
+
+    string mSpecName;
+    double mSpecBonusStability;
+
+public:
+    string  setmName(set &rarg){
+        string mName = arg;
+    }
+
+    string  setmSpecBonusStability(set &rarg){
+        string mName = arg;
+    }
+};
+
+Specialisation:Sharpshooter{
+
+               }
